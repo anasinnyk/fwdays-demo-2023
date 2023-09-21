d9e7abd5031478daa6716af89d8f2785d47ee3cb
Andrii Nasinnyk <anasinnyk@macpaw.com>
2023-09-21 03:50:48 +0300

    feat: go app

--- apps/greater/main.go
+++ apps/greater/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Person struct {
+	Name string
+}
+
 func main() {
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", r.URL.Path)
+		var p Person
+
+		err := json.NewDecoder(r.Body).Decode(&p)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+			return
+		}
+
+		fmt.Fprintf(w, "Hello, %v", p.Name)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
